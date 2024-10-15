8d4df1c3a81a0a786ae0803a1ca48d426193256c
katabondarenko <katushasam@rambler.ru>
2024-10-15 20:45:36 +0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Serviceable car = new Car("car1");
+        Serviceable car2 = new Car("car2");
+
+        Serviceable truck = new Truck("truck11",6);
+        Serviceable truck2 = new Truck("truck2", 8);
+
+        Serviceable bicycle = new Bicycle("bicycle1",2);
+        Serviceable bicycle2 = new Bicycle("bicycle2",2);
+
+
+        ServiceStation station = new ServiceStation();
+        station.check(truck,bicycle,car);
     }
 }
